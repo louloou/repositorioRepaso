cd199d7bd86b1b920c96110992976b77ee0fbc25
alumno <alumno@Instituto.local>
2025-03-13 11:16:58 -0300

    dos

---
+++ java.js
@@ -0,0 +1,8 @@
+let añoNacimiento = 2008
+let añoActual = 2025
+
+function resta  (añoNacimiento, añoActual){
+    return "Mi edad" + (añoActual - añoNacimiento)
+}
+
+alert('Bienvenido/a mi página web, ' + nombre + apellido)
